c2aca212ca54756d039819e42fa43fb173f55251
Rakha <rkhdinar00@gmail.com>
2025-02-20 20:26:58 +0700

    layouting done

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,160 +1,139 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
-    // Efek parallax untuk hero
-    function parallaxEffect() {
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: ".parallax-container",
-                start: "top top",
-                end: "500% top",
-                scrub: 3,
-                pin: true,
-            },
-        })
+    // Lenis smooth scroll setup
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.add((time) => lenis.raf(time * 1000));
+    gsap.ticker.lagSmoothing(0);
 
-            .to(".parallax-container", {
-                scale: 1.2,
-                duration: 1.5,
-                ease: "power1.out",
-            })
-
-            // .to(
-            //     "#text",
-            //     {
-            //         marginTop: "1250px",
-            //     },
-            //     "<"
-            // )
-
-            // .to(
-            //     "#leaf",
-            //     {
-            //         top: "-750px",
-            //         left: "750px",
-            //     },
-            //     "<"
-            // )
-
-            .to(
-                "#tanah2",
-                {
-                    left: "750px",
-                },
-                "<"
-            )
-
-            .to(
-                "#plant2",
-                {
-                    left: "-750px",
-                },
-                "<"
-            )
-
-            .to(
-                "#gunung2",
-                {
-                    top: "500px",
-                },
-                "<"
-            )
-            .to(
-                "#plant1",
-                {
-                    left: "750px",
-                },
-                "<"
-            )
-
-            // .fromTo(
-            //     "#tree",
-            //     {
-            //         y: 500,
-            //     },
-            //     {
-            //         y: -100,
-            //         duration: 1.5,
-            //         ease: "power2.out",
-            //     }
-            // )
-
-            .to("#tree", {
-                scale: 50,
-                duration: 5,
-                ease: "power1.inOut",
-            });
-    }
-
-    // Animasi sticky cards dengan ScrollTrigger dan Lenis
-    function animateStickyCards() {
-        gsap.from(".steps div", {
-            opacity: 0,
-            duration: 1,
-            ease: "power2.inOut",
-            scrollTrigger: {
-                trigger: ".steps",
-                start: "top top",
-                end: "bottom top",
-                scrub: 3,
-            },
-        });
-
-        const lenis = new Lenis();
-        lenis.on("scroll", ScrollTrigger.update);
-        gsap.ticker.add((time) => lenis.raf(time * 1000));
-        gsap.ticker.lagSmoothing(0);
-
-        const stickySection = document.querySelector(".steps");
-        const stickyHeight = window.innerHeight * 7;
-        const cards = document.querySelectorAll(".card");
-        const totalCards = cards.length;
-
-        ScrollTrigger.create({
-            trigger: stickySection,
+    // Parallax animation
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".parallax-container",
             start: "top top",
-            end: `+=${stickyHeight}px`,
+            end: "500% top",
+            scrub: 3,
             pin: true,
-            pinSpacing: true,
-            onUpdate: (self) => positionCards(self.progress),
+        },
+    })
+        .to(".parallax-container", {
+            scale: 1.2,
+            duration: 1.5,
+            ease: "power1.out",
+        })
+        .to(
+            "#tanah2",
+            {
+                left: "750px",
+            },
+            "<"
+        )
+        .to(
+            "#plant2",
+            {
+                left: "-750px",
+            },
+            "<"
+        )
+        .to(
+            "#gunung2",
+            {
+                top: "500px",
+            },
+            "<"
+        )
+        .to(
+            "#plant1",
+            {
+                left: "750px",
+            },
+            "<"
+        )
+        .to("#tree", {
+            scale: 50,
+            duration: 5,
+            ease: "power1.inOut",
         });
 
-        const getRadius = () =>
-            window.innerWidth < 900
-                ? window.innerWidth * 7.5
-                : window.innerWidth * 2.5;
-        const arcAngle = Math.PI * 0.4;
-        const startAngle = Math.PI / 2 - arcAngle / 2;
+    // Steps cards animation
+    gsap.from(".steps div", {
+        opacity: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+            trigger: ".steps",
+            start: "top top",
+            end: "bottom top",
+            scrub: 3,
+        },
+    });
 
-        function positionCards(progress = 0) {
-            const radius = getRadius();
-            const totalTravel = 1 + totalCards / 7.5;
-            const adjustedProgress = (progress * totalTravel - 1) * 0.75;
+    // Sticky cards setup
+    const stickySection = document.querySelector(".steps");
+    const stickyHeight = window.innerHeight * 7;
+    const cards = document.querySelectorAll(".card");
+    const totalCards = cards.length;
 
-            cards.forEach((card, i) => {
-                const normalizedProgress = (totalCards - 1 - i) / totalCards;
-                const cardProgress = normalizedProgress + adjustedProgress;
-                const angle = startAngle + arcAngle * cardProgress;
-                const x = Math.cos(angle) * radius;
-                const y = Math.sin(angle) * radius;
-                const rotation = (angle - Math.PI / 2) * (180 / Math.PI);
+    ScrollTrigger.create({
+        trigger: stickySection,
+        start: "top top",
+        end: `+=${stickyHeight}px`,
+        pin: true,
+        pinSpacing: true,
+        onUpdate: (self) => positionCards(self.progress),
+    });
 
-                gsap.set(card, {
-                    x: x,
-                    y: -y + radius,
-                    rotation: -rotation,
-                    transformOrigin: "center center",
-                });
+    // Cards positioning helper functions
+    const getRadius = () =>
+        window.innerWidth < 900
+            ? window.innerWidth * 7.5
+            : window.innerWidth * 2.5;
+    const arcAngle = Math.PI * 0.4;
+    const startAngle = Math.PI / 2 - arcAngle / 2;
+
+    function positionCards(progress = 0) {
+        const radius = getRadius();
+        const totalTravel = 1 + totalCards / 7.5;
+        const adjustedProgress = (progress * totalTravel - 1) * 0.75;
+
+        cards.forEach((card, i) => {
+            const normalizedProgress = (totalCards - 1 - i) / totalCards;
+            const cardProgress = normalizedProgress + adjustedProgress;
+            const angle = startAngle + arcAngle * cardProgress;
+            const x = Math.cos(angle) * radius;
+            const y = Math.sin(angle) * radius;
+            const rotation = (angle - Math.PI / 2) * (180 / Math.PI);
+
+            gsap.set(card, {
+                x: x,
+                y: -y + radius,
+                rotation: -rotation,
+                transformOrigin: "center center",
             });
-        }
-
-        positionCards(0);
-
-        cards.forEach((card) => observer.observe(card));
-        window.addEventListener("resize", () => positionCards(0));
+        });
     }
 
-    // Hanya memanggil parallaxEffect, animateStickyCards akan dipanggil setelah parallax selesai
-    parallaxEffect();
-    animateStickyCards();
+    positionCards(0);
+    window.addEventListener("resize", () => positionCards(0));
+
+    // Swiper slider setup
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        allowTouchMove: false,
+    });
+
+    document.querySelector('.button-prev').addEventListener('click', () => {
+        swiper.slidePrev();
+    });
+
+    document.querySelector('.button-next').addEventListener('click', () => {
+        swiper.slideNext();
+    });
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,9 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
   {{-- google font end --}}
 
   {{-- my js start --}}
@@ -31,7 +33,7 @@
       </a>
     </div>
     <div class="w-full min-h-min">
-      <nav class="navigation flex">
+      <nav class="flex navigation">
         <a href="" class="active a-nav">ABOUT</a>
         <a href="" class="a-nav">NETWORK</a>
         <a href="" class="a-nav">NEWS</a>
@@ -39,7 +41,7 @@
         <a href="" class="a-nav">CONTACT</a>
       </nav>
     </div>
-    <div class="w-full flex justify-end">
+    <div class="flex justify-end w-full">
       <h1 class="tracking-wider font-ibm">PT. MEDIA TREN IDEA</h1>
     </div>
   </header>
@@ -66,14 +68,15 @@
     <img src="{{ asset('images/plant1.png') }}" alt="" id="plant1" class="img-parallax" />
   </section>
 
-  <section class="relative w-full h-screen overflow-hidden steps bg-[#257c76]">
+  <section class="bg-[#257c76] relative w-full h-screen overflow-hidden steps">
     <div class="step-counter w-full relative flex px-[2em] flex-row m-[2em] justify-between items-center">
       <div class="counter-title relative h-[150px] overflow-hidden w-full flex flex-col items-center">
         <h1
           class="w-max relative text-white font-jakarta font-normal text-xl will-change-transform text-center rounded-3xl outline outline-2 outline-[#E1E1E1] p-2 mt-2">
           Our services
         </h1>
-        <p class="pt-8 text-3xl font-medium text-center text-white">Here’s <span class="text-[#67fbf8]">our services</span> that
+        <p class="pt-8 text-3xl font-medium text-center text-white">Here's <span class="text-[#67fbf8]">our
+            services</span> that
           may helps</p>
         <p class="text-3xl font-medium text-center text-white">with ur business</p>
       </div>
@@ -166,43 +169,279 @@
     </div>
   </section>
 
-  <section class="bg-[#257c76] w-full">
-    <div class="container max-h-screen overflow-hidden w-full flex flex-col items-center pt-4 pb-10">
-      <div class="w-full flex flex-col items-center">
+  <section class="bg-[#257c76] w-full h-screen items-center justify-center">
+    <div class="container flex flex-col items-center w-full max-h-screen pt-4 pb-10 overflow-hidden">
+      <div class="flex flex-col items-center w-full">
         <p
           class="text-white max-w-lg w-fit font-jakarta font-normal text-xl text-center rounded-3xl outline outline-2 outline-[#E1E1E1] p-2 mt-2">
           Customer support
         </p>
-        <h1 class="text-4xl text-white pt-8 font-semibold">Manage customer queries</h1>
-        <h1 class="text-4xl text-white font-semibold">in seconds with metri</h1>
+        <h1 class="pt-8 text-4xl font-semibold text-white">Manage customer queries</h1>
+        <h1 class="text-4xl font-semibold text-white">in seconds with metri</h1>
       </div>
-  
-      <div class="container flex justify-center items-center h-full my-10">
-        <div class="text-center mx-4">
+
+      <div class="container flex items-center justify-center h-full my-10">
+        <div class="mx-4 text-center">
           <h1 class="text-4xl font-semibold text-white">99.9%</h1>
           <p class="pt-4 text-white">Of customers said using Flinta helped them to better service clients.</p>
         </div>
-        <div class="text-center mx-4">
+        <div class="mx-4 text-center">
           <h1 class="text-4xl font-semibold text-white">'24</h1>
-          <p class="pt-4 text-white">Founded in 2024, we’re on a growth journey focused on innovation.</p>
+          <p class="pt-4 text-white">Founded in 2024, we're on a growth journey focused on innovation.</p>
         </div>
-        <div class="text-center mx-4">
+        <div class="mx-4 text-center">
           <h1 class="text-4xl font-semibold text-white">20+</h1>
           <p class="pt-4 text-white">Average monthly clients since we launched in June 2023.</p>
         </div>
-        <div class="text-center mx-4">
+        <div class="mx-4 text-center">
           <h1 class="text-4xl font-semibold text-white">0.05+</h1>
-          <p class="pt-4 text-white">Of tickets left unresolved using metri’s advanced functionality.</p>
+          <p class="pt-4 text-white">Of tickets left unresolved using metri's advanced functionality.</p>
         </div>
       </div>
 
-      <div class="w-full flex flex-col items-center">
-        <h1 class="text-xl text-center text-white font-medium">Explore our Advance System Functionality and Experience the Difference</h1>
-        <h1 class="text-xl text-center text-white font-medium">with metri.</h1>
+      <div class="flex flex-col items-center w-full">
+        <h1 class="text-xl font-medium text-center text-white">Explore our Advance System Functionality and Experience
+          the Difference</h1>
+        <h1 class="text-xl font-medium text-center text-white">with metri.</h1>
       </div>
     </div>
   </section>
-  
+
+  <section class="w-full h-full">
+    <video autoplay loop muted>
+      <source src="{{ asset('videos/placeholder.mp4') }}" type="video/mp4">
+    </video>
+  </section>
+
+  <div class="flex flex-row items-center w-full bg-[#045F50]">
+    <p class="px-10 text-lg font-thin text-white whitespace-nowrap">
+      Our partners
+    </p>
+    <div class="w-auto overflow-hidden">
+      <div class="flex flex-row animate-scroll w-max">
+        <img src="{{ asset('images/logo/logo-metri-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <!-- Duplikat logo untuk efek loop -->
+        <img src="{{ asset('images/logo/logo-metri-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+        <img src="{{ asset('images/logo/logo-putih-baru.png') }}" class="w-52">
+      </div>
+    </div>
+  </div>
+
+  <section class="bg-[#257c76] w-full h-screen">
+    <div class="flex flex-col justify-center h-full gap-5 px-10">
+      <h2 class="py-1 font-thin text-white px-7 w-max font-jakarta rounded-3xl outline outline-1">
+        Our clients
+      </h2>
+      <p class="w-1/2 text-3xl text-white font-jakarta">
+        See what our clients have to say about partnering with us on several projects
+      </p>
+      <div class="w-full swiper">
+        <div class="swiper-wrapper">
+          <div class="w-full swiper-slide">
+            <div class="flex flex-row items-center gap-5">
+              <img src="{{ asset('images/card-1.jpg') }}" class="w-20 h-20 bg-white rounded-full aspect-square">
+              <p class="w-full text-justify text-white uppercase font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div class="flex flex-col gap-3 my-14">
+              <p class="text-justify text-white font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam et libero inventore
+                provident nostrum possimus quidem culpa dolorum dolorem?
+              </p>
+              <p class="text-white font-jakarta text-end">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div class="my-5 border-t-4 border-white"></div>
+            <div class="relative">
+              <p class="absolute bottom-0 left-0 p-5 text-xl font-bold text-white font-jakarta">
+                VINCERE STUDIO
+              </p>
+              <img src="{{ asset('images/card-1.jpg') }}" class="object-cover w-full h-28 photo-card">
+            </div>
+          </div>
+          <div class="w-full swiper-slide">
+            <div class="flex flex-row items-center gap-5">
+              <img src="{{ asset('images/card-1.jpg') }}" class="w-20 h-20 bg-white rounded-full aspect-square">
+              <p class="w-full text-justify text-white uppercase font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div class="flex flex-col gap-3 my-14">
+              <p class="text-justify text-white font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam et libero inventore
+                provident nostrum possimus quidem culpa dolorum dolorem?
+              </p>
+              <p class="text-white font-jakarta text-end">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div class="my-5 border-t-4 border-white"></div>
+            <div class="relative">
+              <p class="absolute bottom-0 left-0 p-5 text-xl font-bold text-white font-jakarta">
+                VINCERE STUDIO
+              </p>
+              <img src="{{ asset('images/card-1.jpg') }}" class="object-cover w-full h-28 photo-card">
+            </div>
+          </div>
+          <div class="w-full swiper-slide">
+            <div class="flex flex-row items-center gap-5">
+              <img src="{{ asset('images/card-1.jpg') }}" class="w-20 h-20 bg-white rounded-full aspect-square">
+              <p class="w-full text-justify text-white uppercase font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div class="flex flex-col gap-3 my-14">
+              <p class="text-justify text-white font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam et libero inventore
+                provident nostrum possimus quidem culpa dolorum dolorem?
+              </p>
+              <p class="text-white font-jakarta text-end">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div class="my-5 border-t-4 border-white"></div>
+            <div class="relative">
+              <p class="absolute bottom-0 left-0 p-5 text-xl font-bold text-white font-jakarta">
+                VINCERE STUDIO
+              </p>
+              <img src="{{ asset('images/card-1.jpg') }}" class="object-cover w-full h-28 photo-card">
+            </div>
+          </div>
+          <div class="w-full swiper-slide">
+            <div class="flex flex-row items-center gap-5">
+              <img src="{{ asset('images/card-1.jpg') }}" class="w-20 h-20 bg-white rounded-full aspect-square">
+              <p class="w-full text-justify text-white uppercase font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div class="flex flex-col gap-3 my-14">
+              <p class="text-justify text-white font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam et libero inventore
+                provident nostrum possimus quidem culpa dolorum dolorem?
+              </p>
+              <p class="text-white font-jakarta text-end">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div class="my-5 border-t-4 border-white"></div>
+            <div class="relative">
+              <p class="absolute bottom-0 left-0 p-5 text-xl font-bold text-white font-jakarta">
+                VINCERE STUDIO
+              </p>
+              <img src="{{ asset('images/card-1.jpg') }}" class="object-cover w-full h-28 photo-card">
+            </div>
+          </div>
+          <div class="w-full swiper-slide">
+            <div class="flex flex-row items-center gap-5">
+              <img src="{{ asset('images/card-1.jpg') }}" class="w-20 h-20 bg-white rounded-full aspect-square">
+              <p class="w-full text-justify text-white uppercase font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div class="flex flex-col gap-3 my-14">
+              <p class="text-justify text-white font-jakarta">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam et libero inventore
+                provident nostrum possimus quidem culpa dolorum dolorem?
+              </p>
+              <p class="text-white font-jakarta text-end">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+            <div class="my-5 border-t-4 border-white"></div>
+            <div class="relative">
+              <p class="absolute bottom-0 left-0 p-5 text-xl font-bold text-white font-jakarta">
+                VINCERE STUDIO
+              </p>
+              <img src="{{ asset('images/card-1.jpg') }}" class="object-cover w-full h-28 photo-card">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-row justify-center gap-5 overflow-hidden">
+        <div class="p-2 border border-white rounded-full button-prev">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-chevron-left"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+          </svg>
+        </div>
+        <div class="p-2 border border-white rounded-full button-next">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-chevron-right"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="w-full py-10 bg-center bg-no-repeat bg-cover shadow-[0_-15px_40px_-15px_rgba(0,0,0,0.3)]"
+    style="background-image: url('{{ asset('images/footer.png') }}')">
+    <div class="flex flex-row gap-20 px-10">
+      <div class="flex flex-col pr-20">
+        <img src="{{ asset('images/logo/logo-putih.png') }}" class="w-28">
+        <p class="font-thin text-white">
+          &copy; 2025 Metri. All rights reserved.
+        </p>
+      </div>
+      <div class="flex flex-col max-w-xs gap-3">
+        <p class="font-bold text-white">
+          Address:
+        </p>
+        <p class="text-white">
+          Jl. Tebet Raya No. 42, RT.5/RW.9, Tebet Timur, Kec. Tebet, Kota Jakarta Selatan, DKI Jakarta 12820
+        </p>
+        <p class="font-bold text-white">
+          Office Hours:
+        </p>
+        <p class="text-white">
+          Mon - Fri: 08:00 - 17:00 <br>
+          Sat - Sun: 08:00 - 12:00
+        </p>
+      </div>
+      <div class="flex flex-col gap-3">
+        <a href="#" class="text-white">
+          About
+        </a>
+        <a href="#" class="text-white">
+          Projects
+        </a>
+        <a href="#" class="text-white">
+          Team
+        </a>
+        <a href="#" class="text-white">
+          Contact Us
+        </a>
+      </div>
+      <div class="flex flex-col gap-3">
+        <a href="#" class="text-white">
+          Instagram
+        </a>
+        <a href="#" class="text-white">
+          X
+        </a>
+        <a href="#" class="text-white">
+          Facebook
+        </a>
+      </div>
+    </div>
+  </footer>
+
 
 
   {{-- cdn gsap & lenis start --}}
