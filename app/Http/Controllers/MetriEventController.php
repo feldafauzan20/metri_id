f0e31f804a93e293660b0bc46d137f822c459e2d
feldafauzan20 <feldafauzan20@gmail.com>
2025-02-27 23:54:19 +0700

    p

--- app/Http/Controllers/MetriEventController.php
+++ app/Http/Controllers/MetriEventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MetriEvent;
 use Illuminate\Http\Request;
 
 class MetriEventController extends Controller
--- app/Http/Controllers/MetriEventPostController.php
+++ app/Http/Controllers/MetriEventPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MetriEventPost;
 use Illuminate\Http\Request;
 
 class MetriEventPostController extends Controller
